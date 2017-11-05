0b1cb3067c6274345ebab4c5742bfc4321db23d0
Ashraful Islam <ashraful@field.buzz>
2017-11-05 16:25:07 +0600

    Removed name from urls. Reason, not to show in permission urls

--- drf_role/urls.py
+++ drf_role/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from drf_role.views import RoleView, PermissionView, AccessControlView, AllUrlList
 
 urlpatterns = [
-    url(r'^roles/', RoleView.as_view(), name='role-view'),
-    url(r'^permissions/', PermissionView.as_view(), name='permission-view'),
-    url(r'^accesses/', AccessControlView.as_view(), name='access-control-view'),
-    url(r'^all-urls/', AllUrlList.as_view(), name='url-list'),
+    url(r'^roles/', RoleView.as_view()),
+    url(r'^permissions/', PermissionView.as_view()),
+    url(r'^accesses/', AccessControlView.as_view()),
+    url(r'^all-urls/', AllUrlList.as_view()),
 ]
