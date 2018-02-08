a529f6c88102c3a450ae0c4a94805623e1c83554
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-02-08 15:25:49 +0600

    Updated to django 2.0 Moved get_urls function to utility directory

--- drf_role/utils/url_utilities.py
+++ drf_role/utils/url_utilities.py
@@ -1,11 +1,23 @@
 from django.conf import settings
-from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
-from django.urls.base import reverse
-from django.urls.exceptions import NoReverseMatch
+from django.urls import URLResolver, URLPattern
+from django.urls.base import resolve, reverse_lazy
 
 __author__ = 'Ashraful'
 
 URL_NAMES = []
+
+
+def get_view_by_url(url_name=None):
+    """
+    **view generator**
+    :param url_name: get url_name as string
+    :return: view function (Though it is class or something)
+    """
+    if not url_name:
+        return None
+    url = reverse_lazy(url_name)
+    resolver_match = resolve(url)
+    return resolver_match.func
 
 
 def get_all_url_names(urlpatterns):
@@ -15,14 +27,14 @@ def get_all_url_names(urlpatterns):
     """
     for pattern in urlpatterns:
         # Check it resolver or pattern. If pattern then end level if not ? then, go deeper level
-        if isinstance(pattern, RegexURLResolver):
+        if isinstance(pattern, URLResolver):
             # Check if the url custom url or django default. Actually making diff by namespace.
             # So, You are not allowed to put namespace on your urls
             if pattern.namespace is not None:
                 continue
             # Recursive call for going deeper to find out regex patterns
             get_all_url_names(pattern.url_patterns)  # call this function recursively
-        elif isinstance(pattern, RegexURLPattern):
+        elif isinstance(pattern, URLPattern):
             url_name = pattern.name  # get the view name
             if url_name:
                 URL_NAMES.append(url_name)
